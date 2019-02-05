8112d6bebf1b6a4445f787c0ca977b09934b9e9f
DJ Shin <djshin@DJs-MacBook-Pro.local>
2019-02-04 19:32:46 -0500

    facebook authentication done

--- App.js
+++ App.js
@@ -1,30 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
 import {f, auth, database} from './config/config'
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      loggedin: false
+    };
     // this.registerUser('fakemail@hotmail.com', 'fakepassword');
-    
-    // auth.signOut()
-    // .then(() => {
-    //   console.log('logged out foo!!!...');
-    // }).catch((error) => {
-    //   console.log('Error:', error);
-    // });
-
+    var that = this;
 
     f.auth().onAuthStateChanged(function(user){
       if(user){
+        that.setState({
+          loggedin: true
+        });
         console.log('logged in foo!', user)
       }else{
+        that.setState({
+          loggedin: false
+        })
         console.log('logged out foo!')
       }
     });
   }
 
+  loginUser = async(email, pass) => {
+    if(email != '' && pass != ''){
+      try{
+        let user = await auth.signInWithAndPassword(email, pass);
+        console.log(user);
+      } catch(error){
+         console.log(error);
+      }
+    } else{
+      alert('missing e-mail or password');
+    }
+  }
 
+   async logInWithFacebook (){
+    const { type, token} = await Expo.Facebook.logInWithReadPermissionsAsync(
+      '398906487536671', 
+      { permissions: ['email', 'public_profile']}
+    );
+    if(type === 'success'){
+      const credentials = f.auth.FacebookAuthProvider.credential(token); 
+      f.auth().signInAndRetrieveDataWithCredential(credentials).catch((error) => {
+        console.log('Error..', error);
+      })
+    }
+  } 
+
+ 
+
+  
 registerUser = (email, password) => {
 
  
@@ -34,15 +64,54 @@ registerUser = (email, password) => {
   .catch((error) =>  console.log('error logging in foo!!' , error));
 }
 
+signUserOut= () => {
+  auth.signOut()
+    .then(() => {
+      console.log('logged out foo!!!...');
+    }).catch((error) => {
+      console.log('Error:', error);
+    });
+}
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>-----------</Text>
+        {this.state.loggedin == true ? ( 
+          <View>
+            <TouchableHighlight onPress={() => this.signUserOut() } style={{ backgroundColor:'red'}}>
+            <Text>Log out</Text>
+            </TouchableHighlight>
+            <Text>Logged in...</Text>
+          </View>
+        ) : (
+          <View>
+
+          
+
+            <TouchableHighlight 
+            onPress={()=> this.setState({emailloginView: true})}
+            style={{backgroundColor: 'green'}}>
+            <Text style={{color:'white'}}>Log In With E-mail</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight 
+            onPress={()=> this.logInWithFacebook( )}
+            style={{backgroundColor: 'green'}}>
+            <Text style={{color:'white'}}>Log In With Facebook</Text>
+            </TouchableHighlight>
+         
+          </View>
+        )}
+          
       </View>
     );
   }
 }
 
+
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -51,3 +120,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+  
